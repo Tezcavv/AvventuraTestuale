cf1c4a0d6b0736f0b475be3c48e58f594f802748
Tezca <vittoriovernassa@gmail.com>
2023-01-26 12:32:51 +0100

    Aggiungere i file di progetto.

---
+++ AvventuraTestuale/Npc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvventuraTestuale {
+    public class Npc {
+
+        private string name;
+        private string dialogue;
+
+        public Npc(string name, string dialogue) {
+            this.name = name;
+            this.dialogue = dialogue;
+        }
+
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Dialogue {
+            get { return dialogue; }
+            set { dialogue = value; }
+        }
+
+    }
+}
+
---
+++ AvventuraTestuale/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvventuraTestuale {
+    public class Player {
+
+        private Vector2 position;
+
+        public Player() { 
+            position= new Vector2(0,0);
+        }
+
+        public Player(Vector2 position) {
+            Position = position;
+        }
+
+        public void Move(Vector2 position) {
+            Console.WriteLine("Moving from " + Position.ToString() + " to " + position.ToString());
+            Position = position;
+            
+        }
+
+        public Vector2 Position {
+            get { return position; }
+            private set { position = value; }
+        }
+    }
+}
